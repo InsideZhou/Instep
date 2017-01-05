efd5e1f94547f815a95c07f634bffe20f3aac3aa
Inside <inside@insidezhou.me>
2017-01-05 17:35:25 +0800

    add more related column types in create table test.

--- orm/src/test/kotlin/instep/orm/AccountTable.kt
+++ orm/src/test/kotlin/instep/orm/AccountTable.kt
@@ -1,8 +1,11 @@
 package instep.orm
 
-import instep.orm.sql.*
+import instep.orm.sql.Table
 
 object AccountTable : Table("account") {
-    val id = IntegerColumn("id", IntegerColumnType.Long).primary().autoIncrement()
-    val name = StringColumn("name", StringColumnType.Varchar).nullable(false)
+    val id = autoIncrementLong("id").primary()
+    val name = varchar("name", 256).nullable(false)
+    val balance = numeric("balance", Int.MAX_VALUE, 2).nullable(false)
+    val createdAt = datetime("created_at").nullable(false)
+    val avatar = lob("avatar")
 }
